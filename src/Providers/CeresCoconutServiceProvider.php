90fcbc0589c93fc20e6037a070b3bd5cb444361a
blinkin69 <ch.bleher@gmail.com>
2020-01-29 11:00:18 +0100

    Update CeresCoconutServiceProvider.php

--- src/Providers/CeresCoconutServiceProvider.php
+++ src/Providers/CeresCoconutServiceProvider.php
@@ -27,7 +27,7 @@ class CeresCoconutServiceProvider extends ServiceProvider
     public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
     {
         $enabledOverrides = explode(", ", $config->get("CeresCoconut.templates.override"));
-
+         $this->overrideTemplate("Ceres::Customer/Components/Registration", "CeresCoconut::Customer/Components/Registration"); 
         // Override partials
         $dispatcher->listen('IO.init.templates', function (Partial $partial) use ($enabledOverrides)
         {
